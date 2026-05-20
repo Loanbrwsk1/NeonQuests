ca98d7e4af001df9245bfbb9414734c0578769bf
Loanbrwsk <Loan.Borowski@protonmail.com>
2026-05-20 10:26:44 +0200

    move progress reset

--- index.php
+++ index.php
@@ -3,83 +3,76 @@ session_start();
 
 require "controllers/controller.php";
 
-if(!isset($_SESSION["pseudo"]) && empty($_SESSION["pseudo"]) && isset($_COOKIE["token"]) && !empty($_COOKIE["token"])){
+if (!isset($_SESSION["pseudo"]) && empty($_SESSION["pseudo"]) && isset($_COOKIE["token"]) && !empty($_COOKIE["token"])) {
     AutoLogin();
 }
 
-if(!isset($_GET["page"]) && !empty($_GET["page"]) && isset($_SESSION["pseudo"]) && !empty($_SESSION["pseudo"]) && !isset($_GET["action"]) && empty($_GET["action"])){
+if (!isset($_GET["page"]) && !empty($_GET["page"]) && isset($_SESSION["pseudo"]) && !empty($_SESSION["pseudo"]) && !isset($_GET["action"]) && empty($_GET["action"])) {
     RedirectHome();
 }
 
-if(isset($_GET["page"]) && !empty($_GET["page"]) && isset($_SESSION["pseudo"]) && !empty($_SESSION["pseudo"])){
+if (isset($_GET["page"]) && !empty($_GET["page"]) && isset($_SESSION["pseudo"]) && !empty($_SESSION["pseudo"])) {
     $page = htmlspecialchars($_GET["page"]);
-    if(($page == "login" || $page == "create") && isset($_SESSION["pseudo"])){
+    if (($page == "login" || $page == "create") && isset($_SESSION["pseudo"])) {
         RedirectHome();
-    }
-    else if($page == "question" && (isset($_GET["theme"]) && !empty($_GET["theme"]) && isset($_GET["question"]) && !empty($_GET["question"]))){
+    } else if ($page == "question" && (isset($_GET["theme"]) && !empty($_GET["theme"]) && isset($_GET["question"]) && !empty($_GET["question"]))) {
         $theme = htmlspecialchars($_GET["theme"]);
         $question = htmlspecialchars($_GET["question"]);
-    }
-    else if($page == "intro" && (isset($_GET["theme"]) && !empty($_GET["theme"]))){
+    } else if ($page == "intro" && (isset($_GET["theme"]) && !empty($_GET["theme"]))) {
         $theme = htmlspecialchars($_GET["theme"]);
     }
-}
-else{
+} else {
     $page = "login";
 }
 
-if(isset($_GET["action"]) && !empty($_GET["action"])){
+if (isset($_GET["action"]) && !empty($_GET["action"])) {
     $action = htmlspecialchars($_GET["action"]);
-    if($action == "check-connect"){
+    if ($action == "check-connect") {
         CheckConnect();
     }
-    if($action == "check-create"){
+    if ($action == "check-create") {
         CheckCreate();
-    }
-    else if($action == "logout"){
+    } else if ($action == "logout") {
         LogOut();
-    }
-    else if($action == "delete"){
+    } else if ($action == "delete") {
         CheckDelete();
-    }
-    else if($action == "change-pwd"){
+    } else if ($action == "change-pwd") {
         CheckChangePwd();
-    }
-    else if($action == "check-answer"){
-        if(isset($_GET["theme"]) && !empty($_GET["theme"]) && isset($_GET["question"]) && !empty($_GET["question"])){
+    } else if ($action == "check-answer") {
+        if (isset($_GET["theme"]) && !empty($_GET["theme"]) && isset($_GET["question"]) && !empty($_GET["question"])) {
             CheckAnswer(htmlspecialchars($_GET["theme"]), htmlspecialchars($_GET["question"]));
         }
     }
 }
 
-if($page == "login"){
+if ($page == "login") {
+    $_SESSION['pseudo'] = "Invité";
+    $_SESSION['progress'] = 0;
+    for ($i = 1; $i <= 9; $i++) {
+        $theme = "T" . $i;
+        for ($j = 1; $j <= 5; $j++) {
+            $question = "Q" . $j;
+            $_SESSION[$theme . $question . "D"] = "";
+            $_SESSION[$theme . $question . "V"] = "";
+        }
+    }
     DisplayLoginPage();
-}
-else if($page == "home"){
+} else if ($page == "home") {
     DisplayHome();
-}
-else if($page == "create"){
+} else if ($page == "create") {
     DisplayCreate();
-}
-else if($page == "account"){
+} else if ($page == "account") {
     DisplayAccount();
-}
-else if($page == "introduction"){
+} else if ($page == "introduction") {
     DisplayIntroduction();
-}
-else if($page == "intro"){
+} else if ($page == "intro") {
     DisplayIntro($theme);
-}
-else if($page == "question" && (int)$question <= 5){
+} else if ($page == "question" && (int)$question <= 5) {
     DisplayQuestion($theme, $question);
-}
-else if($page == "theme-finished"){
+} else if ($page == "theme-finished") {
     DisplayThemeFinished();
-}
-else if($page == "finished"){
+} else if ($page == "finished") {
     DisplayFinished();
-}
-else{
+} else {
     Display404();
 }
-
